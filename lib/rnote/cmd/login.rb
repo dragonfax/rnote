19da597606afe054b9a34b739443333c458cb9c3
Jason Stillwell <dragonfax@gmail.com>
2013-03-03 13:57:42 -0800

    improved input method for passwords

--- lib/rnote/cmd/login.rb
+++ lib/rnote/cmd/login.rb
@@ -1,21 +1,28 @@
 
+require 'highline/import'
+
 include GLI::App
 
 desc 'provide rnote credentials'
 command :login do |c|
+  
+  c.desc "username"
   c.flag [:u,:user,:username]
+  
+  c.desc "password (if not provided, will ask)"
   c.flag [:p,:pass,:password]
+  
   c.action do |global_options,options,args|
 		raise unless args.length == 0
 
     if not options[:u]
-      puts "Please enter your username>"
-      options[:u] = STDIN.gets.chomp
+      answer = ask("Enter your username:  ")
+      options[:u] = answer
     end
 
     if not options[:p]
-      puts "Please enter your password>"
-      options[:p] = STDIN.gets.chomp
+      answer = ask("Enter your password:  ") { |q| q.echo = 'x' }
+      options[:p] = answer
     end
 
     $app.auth.login(options[:u],options[:p])
